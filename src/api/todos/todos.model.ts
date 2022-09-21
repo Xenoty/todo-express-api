966c008f216bb0cbdd627c1086727d1dd04cc334
Jacques Olivier <jacqueso.olivier@gmail.com>
2022-09-21 21:57:18 +1200

    refactor: make fields optional

--- src/api/todos/todos.model.ts
+++ src/api/todos/todos.model.ts
@@ -1,25 +1,25 @@
 import * as z from 'zod';
 
 export const BaseObject = z.object({
-  date_created: z.number().default(Date.now()),
-  date_updated: z.number().default(Date.now()),
-  deleted: z.boolean().default(false),
+  date_created: z.number().optional().default(Date.now()),
+  date_updated: z.number().optional().default(Date.now()),
+  deleted: z.boolean().optional().default(false),
 });
 
 export const Todo = z
   .object({
     name: z.string().trim().max(150).min(1),
-    completed: z.boolean().default(false),
-    date_completed: z.number().nullable().default(null),
+    completed: z.boolean().optional().default(false),
+    date_completed: z.number().optional().nullable().default(null),
   })
   .merge(BaseObject);
 
 export const TodoContainer = z
   .object({
     name: z.string().trim().max(150).min(1),
-    todos: z.array(Todo),
-    completed: z.boolean().default(false),
-    date_completed: z.number().nullable().default(null),
+    todos: z.array(Todo).optional().default([]),
+    completed: z.boolean().optional().default(false),
+    date_completed: z.number().optional().nullable().default(null),
   })
   .merge(BaseObject);
 
